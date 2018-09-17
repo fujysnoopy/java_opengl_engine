e6203f4930ff5c5afb34d8e9c9ae8a87fe32808f
ras <ras@DESKTOP-G2OEQ9V>
2018-09-18 01:51:50 +0200

    crear tuto 14 https://www.youtube.com/watch?v=yNYwZMmgTJk&index=14&list=PLRIWtICgwaX0u7Rf9zkZhLoLuZVfUksDP

---
+++ java_opengl_03-Tuto_X v06 - MIO/src/Texture.java
@@ -0,0 +1,15 @@
+
+//import org.
+
+public class Texture {
+	
+	public int loadTexture(String fileName) {
+		Texture texture = null;
+		
+		texture = TextureLoader.getTexture("PNG", newF);
+		
+		return 0;
+		
+	}
+	
+}
